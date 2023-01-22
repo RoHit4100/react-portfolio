b3afae5ef2cc2423c0fb41da05c7f51afa8cf9c4
rohit udamale <rohit@hackur777.hackur>
2023-01-22 18:30:54 +0530

    styles applied to the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './components/header/Header'
+import "./App.css"
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Header />
+        </div>
     )
 }
 
